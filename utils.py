b71f63a0480a8ec52912c31f0576baf002016e44
RoopkumaD <dasroopkumar5@gmail.com>
2024-01-05 22:35:53 +0530

    initial

---
+++ utils.py
@@ -0,0 +1,26 @@
+from random import shuffle
+from typing import List
+
+
+def random_walk(start: int, other_locations: List[int]):
+    walk = "" + str(start)
+    shuffle(other_locations)
+    walk += "".join([str(k) for k in other_locations])
+    return walk
+
+
+def get_neighbours(walk: str):
+    length = len(walk)
+    all_neighbours = [
+        walk[: i + 1] + walk[j:] + walk[i + 1 : j]
+        for i in range(length - 2)
+        for j in range(i + 2, length)
+    ]
+
+    return all_neighbours
+
+
+if __name__ == "__main__":
+    k = random_walk(1, [2, 3, 4])
+    print(k)
+    print(get_neighbours(k))
